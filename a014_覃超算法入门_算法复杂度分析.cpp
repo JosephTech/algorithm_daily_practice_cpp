40fdb692d3719d56c30141592f63a2c8d5700d8d
JSF <64152612+joseph668@users.noreply.github.com>
2021-04-03 16:14:30 +0800

    没有分析

---
+++ a014_覃超算法入门_算法复杂度分析.cpp
@@ -0,0 +1,10 @@
+/*
+算法复杂度：
+	O(1)	 constant complexity 常数复杂度
+	O(log n) logarithmic complexity  对数复杂度
+	O(n)	 linear complexity 线性时间复杂度
+	O(n^2)	 N square complexity 平方
+	O(n^3)	 N square complexity 立方
+	O(2^n)	 exponential growth 指数
+	O(n!)	 factorial 阶乘
+*/
